92d4481e591dd2f05c6a7e356e81adc25440d3a4
stephany221212 <104568205+stephany221212@users.noreply.github.com>
2024-03-15 09:50:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-const botoes = document.querySelectorAll(".botao");
-
+const botoes = document. querySelectorAl1 (" botao");
+ 
 for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+botoes[i].onclick=function(){
 
-        for(let j=0;j<botoes.length;j++){
-
-        }
-
-        botoes[i].classList.add("ativo");
+    for(let j=0;j<botoes.length;j++){
+        botoes[j].classlist.remove("ativo");
     }
-
+    botoes[i].classlist.add("ativo");
 }
+} 
