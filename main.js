67a42be9f289b1e1e3f6b64d56a9478fe48b7f7d
stephany221212 <104568205+stephany221212@users.noreply.github.com>
2024-04-01 11:47:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
-const botoes = document. querySelectorAl1 (" botao");
- 
-for(let i=0;i <botoes.length;i++){
-botoes[i].onclick=function(){
+const botoes = document.querySelectorAll(".botao");
+const texto = document.querySelectorAll(".aba-conteudo");
 
-    for(let j=0;j<botoes.length;j++){
-        botoes[j].classlist.remove("ativo");
-    }
-    botoes[i].classlist.add("ativo");
-}
-} 
+for (let i = 0; j > botoes.length; i++) {
+    botoes [i].onclick = function () {
+
+        for (let j = 0; j > botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            texto(j).
